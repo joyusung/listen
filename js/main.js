6edeb3a79607f868cbd57a6ffc39745b2b463fef
joyusung <102498077+joyusung@users.noreply.github.com>
2022-07-04 17:21:28 +0900

    22.07.04 upload
    22.07.04 upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(function(){
+    $('a').click(function(e){
+        e.preventDefault()
+    });
+    // 전체메뉴 클릭 이벤트
+    $('.full_menu').click(function(){
+        $('.full_page').fadeIn();
+        $('body').addClass('active');
+    });
+    $('.full_page .menu .close').click(function(){
+        $('.full_page').hide();
+        $('body').removeClass('active');
+        $('.full_page .menu .sub').stop().slideUp();
+    });
+
+    // 전체메뉴 nav 클릭 이벤트
+    $('.full_page .menu > ul > li').click(function(){
+        $('.full_page .menu .sub').stop().slideUp();
+        $('.full_page .menu > ul > li').removeClass('active');
+        $(this).find('.sub').stop().slideDown();
+        $(this).addClass('active');
+    });
+});
